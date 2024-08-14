8192d6dcf289ae7d4bec609a937e65562e84eaea
IT24102680 <it24102680@my.sliit.lk>
2024-08-14 09:42:06 +0530

    IT24102680 Lab04Q01

---
+++ Q1/IT24102680Lab4Q1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class IT24102680Lab4Q1
+{
+	public static void main (String[] args)
+   {
+ 
+	Scanner input = new Scanner(System.in);
+	int number;
+
+	System.out.print("Enter a number: ");
+	number = input.nextInt();
+
+	if(number>0)
+	{
+	System.out.println("The number is: Positive");
+	}
+	else if(number<0)
+	{
+	System.out.println("The number is: Negative");
+	}
+	else
+	{
+	System.out.print("The number is Zero");
+	}
+
+    }
+
+}
